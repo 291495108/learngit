6260307d110d99862912c0ad959aab3228a6e8b3
tj <291495108@qq.com>
2017-05-27 02:20:01 +0800

    add a function in the test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,12 @@
-
 #include<iostream>
 uisng namespace std;
+void function();
 int main()
 {
  cout<<"This is a text for learning git to githup"<<endl;
  return 0;
+}
+void function()
+{
+ cout<<"haha"<<endl;
 }
